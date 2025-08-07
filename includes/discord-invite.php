e3962156308335afecc7edbdce2dba5a8fa36f85
Narrrf <markuswrulich@gmail.com>
2025-08-07 18:02:36 +0200

    🔧 Update Discord invite code to 3hRRh3gB

--- includes/discord-invite.php
+++ includes/discord-invite.php
@@ -3,7 +3,7 @@
 // This file provides the Discord invite code from environment variables
 
 // Read the Discord invite code from environment variable with fallback
-$discord_invite_code = getenv('DISCORD_INVITE_CODE') ?: 'hx4EvgBG';
+$discord_invite_code = getenv('DISCORD_INVITE_CODE') ?: '3hRRh3gB';
 
 // Full Discord URL
 $discord_url = "https://discord.gg/$discord_invite_code";
